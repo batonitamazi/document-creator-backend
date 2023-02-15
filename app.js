64a2aaac247829ca0e0f6e519e3d50ecefc393f9
btntazo <tamazimirianashvili@gmail.com>
2023-02-15 22:45:09 +0400

    feat: add logger, config module and mongoose

--- app.js
+++ app.js
@@ -1,7 +1,24 @@
 const express = require('express')
-
+const cors = require("cors")
+const config = require('./utils/config')
+const mongoose = require('mongoose')
+const logger = require("./utils/logger")
 
 const app = express()
+mongoose.set("strictQuery", config.MONGO_URI);
 
+mongoose
+  .connect(config.MONGO_URI)
+  .then(() => {
+    logger.info("Connected to mongoDb");
+  })
+  .catch((error) => {
+    logger.error("error connecting to mongoDb", error.message);
+  });
+
+
+
+
+app.use(cors())
 
 module.exports = app
